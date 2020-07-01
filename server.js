909d158a085916153bbf58234c027ec817911118
abioduncomms <amichael@commsworth.com>
2020-07-01 23:41:47 +0100

    added about us page

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const next = require('next')
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -16,6 +16,11 @@ app.prepare()
 
   server.get('/', (req, res) => {
     const actualPage = '/'
+    app.render(req, res, actualPage, {})
+})
+
+server.get('/about', (req, res) => {
+    const actualPage = '/about'
     app.render(req, res, actualPage, {})
 })
     
